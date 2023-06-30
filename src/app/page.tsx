fcfc043571156e31479a6f4bc1a4f94d2dcc02af
Jerah <jerah_borges@hotmail.com>
2023-06-30 14:58:24 -0300

    feat: Adiciona funções de página separadas no arquivo 'page'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-
-import nlwlogo from '../assets/nlw-spacetime-logo.svg'
+import { Copyright } from '@/components/Copyrigth'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { Signin } from '@/components/Signin'
 
 export default function Home() {
   return (
@@ -13,62 +13,15 @@ export default function Home() {
         {/* Stripe */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
         {/* Sign in */}
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline ">Crie sua conta</span> e salve suas
-            memórias
-          </p>
-        </a>
+        <Signin />
         {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwlogo} alt="NLW SpaceTime" />
-          <div className="max-w-[420px] space-x-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-          <a
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
-            href=""
-          >
-            CADASTRAR LEMBRANÇAS
-          </a>
-        </div>
-        {/* Copyrigth */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="underline hover:text-green-500"
-            href="http://rocketseat.com.br"
-          >
-            Rocketseat.
-          </a>
-        </div>
+        <Hero />
+        {/* Copyright */}
+        <Copyright />
       </div>
-
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16 ">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="#" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ src/components/Copyrigth.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-green-500"
+        href="http://rocketseat.com.br"
+      >
+        Rocketseat.
+      </a>
+    </div>
+  )
+}
